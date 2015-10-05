db34998dc97454258ea989e40ce6652b7603f43c
Conrad Chu <conradchu@conradchu.com>
2015-10-04 19:58:30 -0700

    Fixed paths for after_restart and before_restart

--- opsworks_gracewiki_deploy_scripts/recipes/default.rb
+++ opsworks_gracewiki_deploy_scripts/recipes/default.rb
@@ -2,24 +2,25 @@ include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
+  current_path = deploy[:current_path]
 
   Chef::Log.info("Installing chef deploy scripts to /deploy directory")
 
   # before_restart deploy script
-  template "#{deploy[:deploy_to]}/current/deploy/before_restart.rb" do
+  template "#{current_path}/deploy/before_restart.rb" do
     source "before_restart.rb.erb"  
     mode "0660"
     group deploy[:group]
     owner deploy[:user]
-    variables(:release_path => "#{deploy[:deploy_to]}/current", :environment => deploy[:rails_env])
+    variables(:release_path => current_path, :environment => deploy[:rails_env])
   end
 
   # after_restart deploy script
-  template "#{deploy[:deploy_to]}/current/deploy/after_restart.rb" do
+  template "#{current_path}/deploy/after_restart.rb" do
     source "after_restart.rb.erb"  
     mode "0660"
     group deploy[:group]
     owner deploy[:user]
-    variables(:release_path => "#{deploy[:deploy_to]}/current", :environment => deploy[:rails_env])
+    variables(:release_path => current_path, :environment => deploy[:rails_env])
   end
 end
