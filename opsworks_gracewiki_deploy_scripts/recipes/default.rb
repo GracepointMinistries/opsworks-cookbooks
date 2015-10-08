ad74e8ba2a99f5aae9da56b52137480e628c98e9
Conrad Chu <conradchu@conradchu.com>
2015-10-08 15:33:30 -0700

    Added asset precompile as a before_symlink deploy hook

--- opsworks_gracewiki_deploy_scripts/recipes/default.rb
+++ opsworks_gracewiki_deploy_scripts/recipes/default.rb
@@ -3,8 +3,24 @@ include_recipe "deploy"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
   current_path = deploy[:current_path]
+  shared_path = "#{deploy[:deploy_to]}/shared"
 
   Chef::Log.info("Installing chef deploy scripts to /deploy directory")
+  
+  # create shared directory for assets, if it doesn't exist
+  directory "#{shared_path}/assets" do
+    mode 0770
+    action :create
+    recursive true
+  end
+
+  # before_symlink
+  cookbook_file "#{current_path}/deploy/before_symlink.rb" do
+    owner deploy[:user]
+    group deploy[:group]
+    mode 0655
+    source "before_symlink.rb"            
+  end
 
   # before_restart deploy script
   template "#{current_path}/deploy/before_restart.rb" do
