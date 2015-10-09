35212900fb9e358d0c6557b00c9768dbebe08a8f
Conrad Chu <conradchu@conradchu.com>
2015-10-09 01:34:19 -0700

    Added sudo

--- opsworks_gracewiki_deploy_scripts/files/default/after_restart.rb
+++ opsworks_gracewiki_deploy_scripts/files/default/after_restart.rb
@@ -3,4 +3,4 @@ rails_env = new_resource.environment["RAILS_ENV"]
 run "sudo /etc/init.d/memcached restart &"
 
 # Restart sphinx
-run "cd #{release_path}; bundle exec rake ts:restart RAILS_ENV=#{rails_env}"
+run "cd #{release_path}; sudo bundle exec rake ts:restart RAILS_ENV=#{rails_env}"
